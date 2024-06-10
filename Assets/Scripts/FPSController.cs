c589949d83d48378ea7dcf9013df7f3292e849b3
David Foot <david.foot@outlook.be>
2024-06-10 17:02:35 +0200

    fuck

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -6,16 +6,39 @@ public class FPSController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private Camera m_camera;
+    [SerializeField] private float m_jumpHeight;
+    [SerializeField] private float m_gravity;
+    private float Ymovement;
+    private bool inJump;
+    private float currentYVelocity;
     void Start()
     {
         m_camera = Camera.main;
         m_camera.transform.position = transform.position;
         m_camera.transform.rotation = transform.rotation;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyUp(KeyCode.Space) && !inJump)
+        {
+            inJump = true;
+            currentYVelocity = m_jumpHeight;
+            
+        }
+        if(inJump)
+        {
+            transform.position += currentYVelocity * Vector3.up * Time.deltaTime;
+            //if (currentYVelocity > 0)
+            //{
+            //    currentYVelocity -= m_gravity * Time.deltaTime;
+            //    transform.position -= currentYVelocity * Vector3.up * Time.deltaTime;
+            //}            
+        }
+
+        if (transform.position.y <= 1) inJump = false;
 
         float translation = Input.GetAxis("Vertical") * 10f;
         float rotationX = Input.GetAxis("Mouse X") * 50f;
