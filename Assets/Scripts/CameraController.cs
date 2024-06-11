f2815acff4ec80bf06a22f85aa13bbe558291d3b
David Foot <david.foot@outlook.be>
2024-06-11 11:29:36 +0200

    Add CharacterController movement Mechanics

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,27 +9,11 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         m_camera = Camera.main;
-        m_camera.transform.position = transform.position;
-        m_camera.transform.rotation = transform.rotation;
         Cursor.lockState = CursorLockMode.Locked;
     }
-
     // Update is called once per frame
     void Update()
     {
-        float translation = Input.GetAxis("Vertical") * 10f;
-        float rotationX = Input.GetAxis("Mouse X") * 50f;
-        float straff = Input.GetAxis("Horizontal") * 10f;
-
-        translation *= Time.deltaTime;
-        straff *= Time.deltaTime;
-        rotationX *= Time.deltaTime;
-
-
-        transform.Translate(0, 0, translation);
-        transform.Translate(straff, 0 , 0);
-        transform.Rotate(0, rotationX, 0);
-        m_camera.transform.position = transform.position;
-        transform.rotation = m_camera.transform.rotation;
+        transform.rotation = Quaternion.LookRotation(new Vector3(m_camera.transform.forward.x, 0, m_camera.transform.forward.z));
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float m_jumpHeight;
     public float gravity = -9.81f;
     public float gravityScale = 1f;
     private float m_velocity;
+    private CharacterController playerController;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float translation = Input.GetAxis("Vertical") * 10f;
+        float straff = Input.GetAxis("Horizontal") * 10f;
+
+        // World space To LocalSpace
+        //transform.position += transform.TransformDirection(new Vector3(0, 0, translation * Time.deltaTime));
+        //transform.position += transform.TransformDirection(new Vector3(straff * Time.deltaTime, 0, 0));
+
+        // Mini Teleport en local Space ? 
+
+        //transform.Translate(0, 0, translation * Time.deltaTime);
+        //transform.Translate(straff * Time.deltaTime , 0, 0);
+
+
+        // Using CharacterController
+        //playerController.Move(transform.TransformDirection(new Vector3(0, 0, translation * Time.deltaTime)));
+        //playerController.Move(transform.TransformDirection(new Vector3(straff * Time.deltaTime, 0, 0)));
+
+
         m_velocity += gravity * gravityScale * Time.deltaTime;
         if (IsGrounded() && m_velocity < 0)
         {
@@ -27,12 +46,16 @@ public class NewBehaviourScript : MonoBehaviour
         {
             m_velocity = m_jumpHeight;
         }
-        transform.Translate(new Vector3(0, m_velocity, 0) * Time.deltaTime);
+        Debug.Log("Player Grounded : " + IsGrounded());
+
+        playerController.Move(transform.TransformDirection(new Vector3(straff, m_velocity, translation ) * Time.deltaTime));
 
     }
+    // Inutile avec le CharacterController
+
     private bool IsGrounded()
     {
-        if (Physics.Raycast(transform.position,Vector3.down,1f))
+        if (Physics.Raycast(transform.position,Vector3.down,2f))
         {
             return true;
         }
